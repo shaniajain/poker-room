3959eac56ca2f22db6cdc44e6287e855fc321df9
rajnikhil17 <rajmarpu575@gmail.com>
2020-05-04 15:41:01 +0530

    Update index.js

--- client/index.js
+++ client/index.js
@@ -5,7 +5,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import store from './store/store'
-import Game from './game'
+import Game from './Game'
 
 ReactDOM.render(
   <Provider store={store}>
